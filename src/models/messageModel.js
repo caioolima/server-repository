6931e52504f818826287cd729364cc7da6ce21a0
Caio Costa de Souza Lima <caiolimaa2002@gmail.com>
2024-06-30 01:03:21 -0300

    Update messageModel.js

--- src/models/messageModel.js
+++ src/models/messageModel.js
@@ -1,8 +1,9 @@
+// messageModel.js
 const mongoose = require('mongoose');
 
 const MessageSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Referência ao usuário
-  communityId: { type: mongoose.Schema.Types.ObjectId, ref: 'CommunityUser' }, // Referência à comunidade
+  userId: String,
+  communityId: { type: mongoose.Schema.Types.ObjectId, ref: 'CommunityUser' }, // Adiciona o ID da comunidade
   message: String,
   media: String,
   timestamp: { type: Date, default: Date.now } // Adiciona um campo de timestamp
